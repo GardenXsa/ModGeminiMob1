53da10082a5dda16ce966dd56d33a1390bc46b03
GardenXsa <>
2025-06-19 02:06:35 +0300

    Last Sync: 2025-06-18 23:06:35 (Mobile)

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,29 @@
+package com.example.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "geminimobs";
+    public static final String NAME = "Gemini Mobs";
+    public static final String VERSION = "0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        logger.info("Gemini Mobs mod has been initialized. The future is now.");
+    }
+}
